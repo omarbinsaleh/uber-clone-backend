66d2f306e7da3c0952a2b183c8f19c3a2ab034ee
omarbinsaleh <omarbinsaleh44@gmail.com>
2025-08-10 12:01:26 +0600

    done some modifications in the captainMiddleware.js file

--- middleware/captainMiddleware.js
+++ middleware/captainMiddleware.js
@@ -9,21 +9,21 @@ const authCaptain = async (req, res, next) => {
    // step 1: check if the token is found or not
    const token = req.cookies?.token || req.headers?.authorization?.split(' ')[1];
    if (!token) {
-      return res.status(401).json({message: 'Unauthorized access'});
+      return res.status(401).json({ success: false, message: 'Unauthorized access'});
    }
 
    try {
       // step 2: check if the token is black listed or not
       const isBlackListed = await blacklistTokenModel.findOne({token});
       if (isBlackListed) {
-         return res.status(401).json({message: 'Unauthorized access'});
+         return res.status(401).json({ success: false, message: 'Unauthorized access'});
       } 
 
       // step 3: verify the token and validate the captain
       const decodedObj = jwt.verify(token, process.env.JWT_SECRET);
       const captain = await captainModel.findOne({_id: decodedObj._id});
       if (!captain) {
-         return res.status(401).json({message: 'Unauthorized access'});
+         return res.status(401).json({ success: false, message: 'Unauthorized access'});
       }
 
       // step 4: add the captain information in the request object
@@ -31,7 +31,7 @@ const authCaptain = async (req, res, next) => {
       return next();
 
    } catch (error) {
-      return res.status(401).json({message: 'Unauthorized access', error});
+      return res.status(401).json({ success: false, message: 'Unauthorized access', error});
    }
 }
 
